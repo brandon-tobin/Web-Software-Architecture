909cde2fbee103ec867ffe93949b44286b796888
Brandon Tobin <brandontobin@cox.net>
2016-01-27 10:01:15 -0700

    Created initial advisor class

--- Projects/Grad_Progress/Model/Advisor/advisor.php
+++ Projects/Grad_Progress/Model/Advisor/advisor.php
@@ -6,3 +6,45 @@
  * Student List Model
  *
  */
+
+class Advisor
+{
+    public $advisor_First_Name;
+    public $advisor_Last_Name;
+    public $student_Array;
+
+    public function __construct($id)
+    {
+        if ($id == 1) {
+            $this->create_Peter();
+        }
+        if ($id == 2) {
+            $this->create_James();
+        }
+        if ($id == 3) {
+            $this->create_Brandon();
+        }
+    }
+
+    function create_Peter()
+    {
+        $this->advisor_First_Name = 'Peter';
+        $this->advisor_Last_Name = 'James';
+        $this->student_Array = array("Anne");
+    }
+
+    function create_James()
+    {
+        $this->advisor_First_Name = 'James';
+        $this->advisor_Last_Name = 'Good';
+        $this->student_Array = array("Mike", "Brad", "Neal", "Sam");
+    }
+
+    function create_Brandon()
+    {
+        $this->advisor_First_Name = 'Brandon';
+        $this->advisor_Last_Name = 'Barnes';
+        $this->student_Array = array("Jessica");
+    }
+
+}
