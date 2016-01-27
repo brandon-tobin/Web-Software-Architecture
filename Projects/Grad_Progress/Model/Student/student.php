48bec4c30ed28c5373ad20775da0fde78eb78e79
Brandon Tobin <brandontobin@cox.net>
2016-01-26 22:48:42 -0700

    Created the students class

--- Projects/Grad_Progress/Model/Student/student.php
+++ Projects/Grad_Progress/Model/Student/student.php
@@ -6,3 +6,66 @@
  * Student Forms Model
  *
  */
+
+class Student
+{
+    public $student_First_Name;
+    public $form_count;
+    public $form_Records_Array;
+
+
+    public function __construct($id)
+    {
+        if ($id == 1) {
+            $this->create_Anne();
+        }
+        if ($id == 2) {
+            $this->create_Mike();
+        }
+        if ($id == 3) {
+            $this->create_Brad();
+        }
+        if ($id == 4) {
+            $this->create_Jessica();
+        }
+        if ($id == 5) {
+            $this->create_Neal();
+        }
+        if ($id == 6) {
+            $this->create_Sam();
+        }
+    }
+
+    function create_Anne()
+    {
+        $this->student_First_Name = 'Anne';
+        $this->form_count = 1;
+        $this->form_Records_Array = array("January 18, 2016", "January 18, 2016", "Yes", "Link");
+    }
+
+    function create_Mike()
+    {
+
+    }
+
+    function create_Brad()
+    {
+
+    }
+
+    function create_Jessica()
+    {
+
+    }
+
+    function create_Neal()
+    {
+
+    }
+
+    function create_Sam()
+    {
+
+    }
+}
+
