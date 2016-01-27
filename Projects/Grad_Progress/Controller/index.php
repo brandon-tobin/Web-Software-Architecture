92d1931e5c830743aa36bc0f60a6cbed2f0cde84
Brandon Tobin <brandontobin@cox.net>
2016-01-27 05:55:51 +0000

    New landing page for student roster

---
+++ Projects/Grad_Progress/Controller/index.php
@@ -0,0 +1,110 @@
+ <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd"> 
+
+      <html lang="en"> 
+        <head>
+
+          <!-- Last Updated Spring 2016 --> 
+          <!-- Brandon Tobin --> 
+          <!-- University of Utah --> 
+       
+          <!-- This page contains a list of all graduate students  --> 
+
+
+          <title>Student Roster</title> 
+      
+          <!-- Meta Information about Page -->
+          <meta charset="utf-8"/>
+          <meta name="AUTHOR"      content="Brndon Tobin"/>
+          <meta name="keywords"    content="HTML, Projects"/>
+          <meta name="description" content="List of graduate students"/>
+      
+          <!-- ALL CSS FILES -->
+           <link rel="stylesheet" href="../../../Resources/css/stylesheet.css" type="text/css"/>
+          
+        </head> 
+       
+        <body> 
+
+	 <!-- Header -->
+         <div id="header">
+          <img src="/Resources/Images/uofufootball.jpg" alt="Rice Eccles Stadium" />
+          <h1>University of Utah - CS 4540</h1>
+          <h2>Web Software Architecture - Spring 2016</h2>
+          <h2>Brandon Tobin</h2>
+	  <h2>Grad Progress - Assignment 1</h2>
+         </div>
+ 
+	 <!-- Navigation Bar -->
+	 <ul id="navigation">
+          <li><a href="../../index.html">Home</a></li>
+          <li><a href="../../Projects/">Projects</a></li>
+          <li><a href="../../Class_Examples/">Examples</a></li>
+	 </ul>
+      
+         <h1>Graduate Students</h1>
+     	
+	 <!-- Table containing students --> 
+         <table class="roster">
+	  <tr>
+           <th>Name:</th>
+           <th>Compliance:</th>
+	   <th>Current Form Date:</th>
+           <th>Current Form:</th>
+           <th>Advisor Signature:</th>
+	   <th>Profile</th>
+          </tr>
+          <tr>
+           <td>Anne Smith</td>
+           <td>In</td>
+	   <td>January 18, 2016</td>
+           <td>Current</td>
+           <td>Yes</td>
+	   <td><a href="Student/progress_form.php?id=1">View</a></td>
+          </tr>
+          <tr>
+           <td>Mike Jones</td>
+           <td>Out</td>
+	   <td>December 23, 2014</td>
+           <td>Out of Date</td>
+           <td>No</td>
+	   <td><a href="student_mike_forms_list.html">View</a></td>
+          </tr>
+	  <tr>
+           <td>Brad Rust</td>
+           <td>Out</td>
+           <td>October 20, 2015</td>
+           <td>Out of Date</td>
+           <td>Yes</td>
+           <td><a href="student_brad_forms_list.html">View</a></td>
+          </tr>
+	  <tr>
+           <td>Jessica Brown</td>
+           <td>In</td>
+           <td>January 19, 2016</td>
+           <td>Current</td>
+           <td>Yes</td>
+           <td><a href="student_jessica_forms_list.html">View</a></td>
+          </tr>
+	  <tr>
+           <td>Neal Cates</td>
+           <td>In</td>
+           <td>December 25, 2015</td>
+           <td>Current</td>
+           <td>Yes</td>
+           <td><a href="student_neal_forms_list.html">View</a></td>
+          </tr>
+	  <tr>
+           <td>Sam Bradford</td>
+           <td>Out</td>
+           <td>November 10, 2012</td>
+           <td>Out of Date</td>
+           <td>No</td>
+           <td><a href="student_sam_forms_list.html">View</a></td>
+          </tr>
+
+         </table>
+          
+        </body>
+        
+      </html> 
+      
