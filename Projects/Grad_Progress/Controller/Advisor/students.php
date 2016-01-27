c2aa8b05ee09cb85c8c95d8e56e586a7741026cb
Brandon Tobin <brandontobin@cox.net>
2016-01-27 10:00:57 -0700

    Coded the advisor controller

--- Projects/Grad_Progress/Controller/Advisor/students.php
+++ Projects/Grad_Progress/Controller/Advisor/students.php
@@ -6,3 +6,18 @@
  * Student List Controller
  *
  */
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+set_include_path("../../Model/Advisor/" .PATH_SEPARATOR . "../../View/Advisor/");
+
+require_once 'advisor.php';
+
+$id = $_GET['id'];
+
+$advisor = new Advisor($id);
+
+require "students_view.php";
+
+?>
