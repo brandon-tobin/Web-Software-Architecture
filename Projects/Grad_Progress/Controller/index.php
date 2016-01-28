70409e0903d88341e5f5b8e5c38dbb637a0a00dd
Brandon Tobin <brandontobin@cox.net>
2016-01-27 21:01:38 -0700

    Updated link

--- Projects/Grad_Progress/Controller/index.php
+++ Projects/Grad_Progress/Controller/index.php
@@ -52,8 +52,8 @@
 
             <ul>
                 <li><a href="../V2/">V2</a></li>
-                <li><a href="/Projects/Grad_Progress/README.html">README - Assignment 2</a></li>
-                <li><a href="/Projects/Grad_Progress/advisors_list.php">View Assignment 2</a></li>
+                <li><a href="../V2/README.html">README - Assignment 2</a></li>
+                <li><a href="../V2/advisors_list.php">View Assignment 2</a></li>
             </ul>
 
         </body>
