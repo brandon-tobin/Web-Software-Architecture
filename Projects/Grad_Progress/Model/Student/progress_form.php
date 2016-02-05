85864bd1e09d508a81f72c795a3c8aa561444454
Brandon Tobin <brandontobin@cox.net>
2016-02-04 18:19:23 -0700

    Trying to calculate number of semesters

--- Projects/Grad_Progress/Model/Student/progress_form.php
+++ Projects/Grad_Progress/Model/Student/progress_form.php
@@ -134,7 +134,7 @@ class Student_Form
             if (strpos($this->semester_Admitted, 'Fall') !== false)
             {
                 $year = substr($this->semester_Admitted, 4, 4);
-                $admit_Date = strtotime("1 June ".substr($this->semester_Admitted, 4, 4));
+                $admit_Date = strtotime("1 June ".substr($this->semester_Admitted, 4, 5));
                 $current_Date = strtotime("today");
                 $elapsed_time = floor((floor(($current_Date - $admit_Date) / 2628000) / 6)) + 1;
                 //$this->num_semesters = $elapsed_time;
