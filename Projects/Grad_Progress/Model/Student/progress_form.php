f18617f9bfc538c85057fec8ddd38a1f75c086a8
Brandon Tobin <brandontobin@cox.net>
2016-02-04 18:04:46 -0700

    Trying to calculate number of semesters

--- Projects/Grad_Progress/Model/Student/progress_form.php
+++ Projects/Grad_Progress/Model/Student/progress_form.php
@@ -133,10 +133,10 @@ class Student_Form
            // $this->num_semesters = $current_Date;
             if (strpos($this->semester_Admitted, 'Fall') !== false)
             {
-                $admit_Date = "June 1, 2015";
+                $admit_Date = "1 June 2015";
             }
             $this->num_semesters = $current_Date;
-            $this->num_semesters = "TEst";
+           // $this->num_semesters = "TEst";
 
 
             $this->uncompletedActivity = array("Identify Advisor", "Program of study approved by advisor and initial committee", "Complete teaching mentorship", "Complete required courses", "Full committee formed", "Program of Study approved by committee", "Written qualifier", "Oral qualifier/Proposal", "Dissertation defense");
