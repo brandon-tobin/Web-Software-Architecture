6080f50ec8c9461a2f5301ef536bffd69bdc1a43
Brandon Tobin <brandontobin@cox.net>
2016-02-11 17:13:39 -0700

    Add MVC for user creation

--- Projects/Grad_Progress/Controller/Student/creation_form.php
+++ Projects/Grad_Progress/Controller/Student/creation_form.php
@@ -14,12 +14,12 @@ set_include_path("../../Model/Student/" .PATH_SEPARATOR . "../../View/Student/")
 require_once 'student.php';
 
 // Get the id out of the url
-$id = $_GET['id'];
+//$id = $_GET['id'];
 
 // Create a new student object
-$student = new Student($id);
+//$student = new Student($id);
 
 // Require the student_forms view for display
-require "student_forms_view.php";
+require "creation_form_view.php";
 
 ?>
