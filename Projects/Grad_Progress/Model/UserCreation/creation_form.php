f6bf79e47d4f6394b1f0b7285aacbd8bf6fa8729
Brandon Tobin <brandontobin@cox.net>
2016-02-16 17:49:46 -0700

    Information validation back end

--- Projects/Grad_Progress/Model/UserCreation/creation_form.php
+++ Projects/Grad_Progress/Model/UserCreation/creation_form.php
@@ -74,6 +74,7 @@ if (isset($_REQUEST['name']) && isset($_REQUEST['uid']) && isset($_REQUEST['user
 
         } catch (PDOException $ex) {
             error_log("Tobin is logging:   " . $ex->getMessage());
+            require '../../View/UserCreation/creation_fail_view.php';
         }
     }
     else
