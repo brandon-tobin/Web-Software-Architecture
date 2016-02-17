a70720fd4cc938caf8989a7659f1899dc10dbd94
Brandon Tobin <brandontobin@cox.net>
2016-02-16 20:31:21 -0700

    Adding additional student info

--- Projects/Grad_Progress/Model/UserCreation/creation_form.php
+++ Projects/Grad_Progress/Model/UserCreation/creation_form.php
@@ -27,7 +27,7 @@ if (isset($_REQUEST['name']) && isset($_REQUEST['uid']) && isset($_REQUEST['user
     $track = trim($_REQUEST['track']);
     $date = trim($_REQUEST['date']);
 
-    error_log('Tobin made it here!!!');
+    error_log('Tobin position is' . $position);
 
     // Perform simple validations
     if ($name == '') {
