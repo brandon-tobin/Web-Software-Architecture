2640555ed406ec2e950ffdab59818421c08032a8
Brandon Tobin <brandontobin@cox.net>
2016-02-17 18:06:36 -0700

    Processs form

--- Projects/Grad_Progress/Model/Student/new_progress_form.php
+++ Projects/Grad_Progress/Model/Student/new_progress_form.php
@@ -2,7 +2,52 @@
 
 if (isset($_POST['submit']))
 {
-    error_log("Made it here TOBIN!!!");
+    $student_ID = $_GET['id'];
+    $advisor = trim($_REQUEST['advisor']);
+    $committee1 = trim($_REQUEST['committee1']);
+    $committee2 = trim($_REQUEST['committee2']);
+    $committee3 = trim($_REQUEST['committee3']);
+    $committee4 = trim($_REQUEST['committee4']);
+    $activity1 = trim($_REQUEST['activity1']);
+    $semester_completed1 = trim($_REQUEST['semester_completed1']);
+    $activity2 = trim($_REQUEST['activity2']);
+    $semester_completed2 = trim($_REQUEST['semester_completed2']);
+    $activity3 = trim($_REQUEST['activity3']);
+    $semester_completed3 = trim($_REQUEST['semester_completed3']);
+    $activity4 = trim($_REQUEST['activity4']);
+    $semester_completed4 = trim($_REQUEST['semester_completed4']);
+    $activity5 = trim($_REQUEST['activity5']);
+    $semester_completed5 = trim($_REQUEST['semester_completed5']);
+    $activity6 = trim($_REQUEST['activity6']);
+    $semester_completed6 = trim($_REQUEST['semester_completed6']);
+    $activity7 = trim($_REQUEST['activity7']);
+    $semester_completed7 = trim($_REQUEST['semester_completed7']);
+    $activity8 = trim($_REQUEST['activity8']);
+    $semester_completed8 = trim($_REQUEST['semester_completed8']);
+    $activity9 = trim($_REQUEST['activity9']);
+    $semester_completed9 = trim($_REQUEST['semester_completed9']);
+    $requirements_met = trim($_REQUEST['requirements_met']);
+    $comments = trim($_REQUEST['comments']);
+
+    // Get highest form number for user
+    $db = new PDO("mysql:host=localhost;dbname=Grad_Prog_V4;charset=utf8", 'Grad_Application', '173620901');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    // Get all of the information required to display the student's progress form.
+    $query = "SELECT COUNT(fid) as fid FROM Forms WHERE uid = $student_ID";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    $form_ID = $result['fid'] + 1;
+
+
+    error_log("Tobin!!! Form ID IS " . $form_ID);
+
+   /* foreach ($result as $row) {
+        $form_ID = $row['fid'];
+    }*/
 }
 
 class New_Student_Form
