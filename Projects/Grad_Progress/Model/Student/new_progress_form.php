7740424f5c0a93f4348bff180a907a72cdd91264
Brandon Tobin <brandontobin@cox.net>
2016-02-17 18:47:13 -0700

    Insert activities

--- Projects/Grad_Progress/Model/Student/new_progress_form.php
+++ Projects/Grad_Progress/Model/Student/new_progress_form.php
@@ -59,15 +59,15 @@ if (isset($_POST['submit']))
     {
         $db->beginTransaction();
         $stmt = $db->prepare("INSERT INTO Activities (sid, activity, date_completed) VALUES
-                              ($student_ID, 'Identify Advisor', $semester_completed1),
-                              ($student_ID, 'Program of study approved by advisor and initial committee', $semester_completed2),
-                              ($student_ID, 'Complete teaching mentorship', $semester_completed3),
-                              ($student_ID, 'Complete required courses', $semester_completed4),
-                              ($student_ID, 'Full committee formed', $semester_completed5),
-                              ($student_ID, 'Program of Study approved by committee', $semester_completed6),
-                              ($student_ID, 'Written qualifier', $semester_completed7),
-                              ($student_ID, 'Oral qualifier/Proposal', $semester_completed8),
-                              ($student_ID, 'Dissertation defense', $semester_completed9)");
+                              ($student_ID, \"Identify Advisor\", $semester_completed1),
+                              ($student_ID, \"Program of study approved by advisor and initial committee\", $semester_completed2),
+                              ($student_ID, \"Complete teaching mentorship\", $semester_completed3),
+                              ($student_ID, \"Complete required courses\", $semester_completed4),
+                              ($student_ID, \"Full committee formed\", $semester_completed5),
+                              ($student_ID, \"Program of Study approved by committee\", $semester_completed6),
+                              ($student_ID, \"Written qualifier\", $semester_completed7),
+                              ($student_ID, \"Oral qualifier/Proposal\", $semester_completed8),
+                              ($student_ID, \"Dissertation defense\", $semester_completed9)");
     }
     else if ($activity8 != 0)
     {
