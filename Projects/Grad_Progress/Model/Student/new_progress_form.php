63c645586ca8c6b1a3fb2d05276fbad4b8bbed71
Brandon Tobin <brandontobin@cox.net>
2016-02-18 22:41:58 -0700

    Changing some text for final submision

--- Projects/Grad_Progress/Model/Student/new_progress_form.php
+++ Projects/Grad_Progress/Model/Student/new_progress_form.php
@@ -209,6 +209,7 @@ class New_Student_Form
                 array_push($this->committee, $row['name']);
             }
 
+            error_log("TOBIN committee contains: " .$this->committee);
 
             $query = "SELECT name FROM Users WHERE position = 'F'";
             $statement = $db->prepare($query);
