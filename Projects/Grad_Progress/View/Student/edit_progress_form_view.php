132456ec0f5b5f152e18aaed36d2b1b6fcd4211b
Brandon Tobin <brandontobin@cox.net>
2016-02-22 15:14:20 -0700

    Implementing Partials

--- Projects/Grad_Progress/View/Student/edit_progress_form_view.php
+++ Projects/Grad_Progress/View/Student/edit_progress_form_view.php
@@ -34,17 +34,11 @@ echo "
 
             </head>
 
-            <body>
+            <body>";
 
-                <!-- Header -->
-                <div id=\"header\">
-                    <img src=\"/Resources/Images/uofufootball.jpg\" alt=\"Rice Eccles Stadium\" />
-                    <h1>University of Utah - CS 4540</h1>
-                    <h2>Web Software Architecture - Spring 2016</h2>
-                    <h2>Brandon Tobin</h2>
-                    <h2>Grad Progress - Assignment 4</h2>
-                </div>
+                require "../partials/header.php";
 
+            echo "
                 <!-- Navigation Bar -->
                 <ul id=\"navigation\">
                     <li><a href=\"../../../../index.html\">Home</a></li>
