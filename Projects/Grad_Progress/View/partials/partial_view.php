addb66318a8559e647a1a1951c4907709c0dafa6
Brandon Tobin <brandontobin@cox.net>
2016-02-22 15:41:32 -0700

    Implementing Partials

---
+++ Projects/Grad_Progress/View/partials/partial_view.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file contains the partial views for simplifying the code in the main view files.
+ */
+
+function getHeader()
+{
+    return "
+    <!-- Header -->
+    <div id=\"header\">
+        <img src=\"../../../../Resources/Images/uofufootball.jpg\" alt=\"Rice Eccles Stadium\" />
+        <h1>University of Utah - CS 4540</h1>
+        <h2>Web Software Architecture - Spring 2016</h2>
+        <h2>Brandon Tobin</h2>
+        <h2>Grad Progress - Assignment 5</h2>
+    </div>";
+}
+
+function getNavigation()
+{
+    return "
+    <!-- Navigation Bar -->
+    <ul id=\"navigation\">
+        <li><a href=\"../../../../index.html\">Home</a></li>
+        <li><a href=\"../../../../Projects/\">Projects</a></li>
+        <li><a href=\"../../../../Class_Examples/\">Examples</a></li>
+    </ul>";
+}
