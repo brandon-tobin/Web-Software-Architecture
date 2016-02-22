0f61c3833a84399b8b8012be3f36fe57936ea061
Brandon Tobin <brandontobin@cox.net>
2016-02-22 16:13:46 -0700

    Implemented DB connection externally

--- Projects/Grad_Progress/Model/db.php
+++ Projects/Grad_Progress/Model/db.php
@@ -2,7 +2,7 @@
 
 function openDBConnection()
 {
-    $DBH = new PDO ("mysql:host=localhost;dbname=Grad_Prog_V4;charset=utf8", 'Grad_Application', '173620901');
+    $DBH = new PDO ("mysql:host=localhost;dbname=Grad_Prog_V5;charset=utf8", 'Grad_Application', '173620901');
     $DBH->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $DBH->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     return $DBH;
