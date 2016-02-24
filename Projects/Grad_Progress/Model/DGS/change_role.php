83e853b657650bb770779e2d34a2c255e7eb816a
Brandon Tobin <brandontobin@cox.net>
2016-02-24 00:31:17 -0700

    Adding functionality to change user role

--- Projects/Grad_Progress/Model/DGS/change_role.php
+++ Projects/Grad_Progress/Model/DGS/change_role.php
@@ -155,7 +155,7 @@ class User_Roles
             $db = openDBConnection();
 
             // Query the database to find out which advisor is related to this student.
-            $query = "SELECT username, role FROM Roles)";
+            $query = "SELECT username, role FROM Roles";
             $statement = $db->prepare($query);
             $statement->execute();
 
