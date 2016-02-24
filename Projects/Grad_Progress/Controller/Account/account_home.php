970a14fd49b4f4dff32f621b5518b9f5ce419af5
Brandon Tobin <brandontobin@cox.net>
2016-02-23 23:32:45 -0700

    Creating an account home for user

--- Projects/Grad_Progress/Controller/Account/account_home.php
+++ Projects/Grad_Progress/Controller/Account/account_home.php
@@ -5,10 +5,6 @@
  * Date: 2/23/2016
  * Time: 10:08 PM
  */
-
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
-
 // Set the include path for the model and view
 set_include_path("../../Model/Account/");
 
