6585fbfdfd9d8c39d7205b039f6a9002f91bf1f6
Brandon Tobin <brandontobin@cox.net>
2016-02-23 22:15:58 -0700

    Creating an account home for user

---
+++ Projects/Grad_Progress/Controller/Account/account_home.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brand
+ * Date: 2/23/2016
+ * Time: 10:08 PM
+ */
+
+// Set the include path for the model and view
+set_include_path("../../Model/Account/");
+
+// Require the model file once
+require_once 'account_home.php';
---
+++ Projects/Grad_Progress/Model/Account/account_home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brand
+ * Date: 2/23/2016
+ * Time: 10:08 PM
+ */
+
+require ('../../Model/Functions/authentication.php');
+
+error_log("Made it here!!!!!!!");
+
+redirectToHTTPS();
+
+verify_Login('');
+
+$role = $_SESSION['roles'];
+
+require ('../../View/Account/account_home.php');
