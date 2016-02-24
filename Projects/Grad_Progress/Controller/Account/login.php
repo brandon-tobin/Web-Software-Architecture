1dc1de20e2431e794052324e7007f75cd96a8ded
Brandon Tobin <brandontobin@cox.net>
2016-02-23 21:18:54 -0700

    Adding login page

---
+++ Projects/Grad_Progress/Controller/Account/login.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: brand
+ * Date: 2/23/2016
+ * Time: 9:18 PM
+ */
+
+// Set the include path for the model and view
+set_include_path("../../Model/UserCreation/" .PATH_SEPARATOR . "../../View/Account/");
+
+// Require the model file once
+require_once 'login.php';
