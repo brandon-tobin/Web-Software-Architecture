59e38605450a07c7fa6acd4bd9e748124965d605
Brandon Tobin <brandontobin@cox.net>
2016-02-25 18:38:35 -0700

    Added functionality to update the new role into the database

--- Projects/Grad_Progress/Model/Functions/authentication.php
+++ Projects/Grad_Progress/Model/Functions/authentication.php
@@ -29,9 +29,9 @@ function redirectToHTTPS()
 {
     if (!usingHTTPS())
     {
-        $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-        header("Location:$redirect");
-        exit();
+        //$redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        //header("Location:$redirect");
+        //exit();
     }
 }
 
