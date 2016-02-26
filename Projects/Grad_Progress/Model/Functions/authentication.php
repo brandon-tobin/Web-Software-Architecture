13851b1342cf4819f631b1d30192fa1c4601bd1e
Brandon Tobin <brandontobin@cox.net>
2016-02-26 16:40:55 -0700

    Creating navbar for project homepage

--- Projects/Grad_Progress/Model/Functions/authentication.php
+++ Projects/Grad_Progress/Model/Functions/authentication.php
@@ -179,6 +179,115 @@ function verify_Login($role)
 
 }
 
+function navBar_Login($role)
+{
+    // Redirect to use HTTPS
+    redirectToHTTPS();
+
+    session_start();
+
+    // Check to see if user is logged in
+    if (isset($_SESSION['userid']))
+    {
+        // Check to see if user belongs to role in parameter
+        if ($role == '' || (isset($_SESSION['roles']) && in_array($role, $_SESSION['roles'])))
+        {
+            error_log("TOBIN User is logged in and Role is correct!!!!");
+            return $_SESSION['realname'];
+        }
+        else{
+            error_log("TOBIN User is logged but the Role is incorrect!!!!");
+            require ('../View/Account/bad_role.php');
+            exit();
+        }
+    }
+
+    // Empty error message
+    $message = '';
+
+    // User is attempting to log in. Need to verify credentials
+    if (isset($_REQUEST['username']) && isset($_REQUEST['password']))
+    {
+        $username = $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+
+
+        try
+        {
+            $DBH = openDBConnection();
+
+            // Get information about the user
+            $stmt = $DBH->prepare("SELECT uid, name, position, password FROM Users WHERE username = ?");
+            $stmt->bindValue(1, $username);
+            $stmt->execute();
+
+            // Was the user real?
+            if ($row = $stmt->fetch())
+            {
+                // Validate the password
+                $hashedPassword = $row['password'];
+                if (computeHash($password, $hashedPassword) == $hashedPassword)
+                {
+                    $_SESSION['userid'] = $row['uid'];
+                    $_SESSION['realname'] = htmlspecialchars($row['name']);
+                    $_SESSION['login'] = $username;
+                    $stmt->closeCursor();
+                    $stmt = $DBH->prepare("SELECT role FROM Roles WHERE username = ?");
+                    $stmt->bindValue(1, $username);
+                    $stmt->execute();
+                    $roles = array();
+                    while ($row = $stmt->fetch())
+                    {
+                        $roles[] = $row['role'];
+                    }
+                    $_SESSION['roles'] = $roles;
+
+                }
+                else
+                {
+                    $message = "Username or password is incorrect.";
+                    require ("../View/Account/login_view.php");
+                    exit();
+                }
+            }
+            else
+            {
+                $message = "Username or password is incorrect.";
+                require ("../View/Account/login_view.php");
+                exit();
+            }
+        }
+        catch (PDOException $ex)
+        {
+            error_log("TOBIN THERE WAS AN ERROR LOOKING UP USER : " . $ex->getMessage());
+            exit();
+        }
+
+        // Logged in so change session ID.
+        changeSessionID();
+        if ($role == '' || in_array($role, $_SESSION['roles']))
+        {
+            error_log("TOBIN RETURNING!!!!!");
+            return;
+        }
+        else
+        {
+            error_log("TOBIN User is logged but the Role is incorrect!!!!");
+            require ('../View/Account/bad_role.php');
+            exit();
+        }
+    }
+    else
+    {
+        // error_log("TOBIN User is logged but the Role is incorrect!!!!");
+        require ("../View/Account/login_view.php");
+        exit();
+    }
+
+}
+
+
+
 // Generate random salt
 function makeSalt()
 {
