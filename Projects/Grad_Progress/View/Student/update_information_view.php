a7e363bae0a7548525d3c0cd11e39910c2c2083d
Brandon Tobin <brandontobin@cox.net>
2016-02-26 15:26:00 -0700

    Added navbar / partials to every view

--- Projects/Grad_Progress/View/Student/update_information_view.php
+++ Projects/Grad_Progress/View/Student/update_information_view.php
@@ -1,27 +1,30 @@
 <?php require('../../View/Partials/partial_view.php'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+
+<html lang="en">
+
+    <head>
+
+        <!-- Last Updated Spring 2016 -->
+        <!-- Brandon Tobin -->
+        <!-- University of Utah -->
+
+        <!-- View for updating user roles  -->
+
+        <title>User Roles</title>
+
+        <!-- Meta Information about Page -->
+        <meta charset="utf-8"/>
+        <meta name="AUTHOR"      content="Brndon Tobin"/>
+        <meta name="keywords"    content="HTML, Projects"/>
+        <meta name="description" content="Landing page for updating user roles"/>
+
         <!-- ALL CSS FILES -->
         <link rel="stylesheet" href="../../../../Resources/css/stylesheet.css" type="text/css"/>
 
     </head>
 
     <body>
-
-        <!-- Header -->
-        <div id="header">
-            <img src="/Resources/Images/uofufootball.jpg" alt="Rice Eccles Stadium" />
-            <h1>University of Utah - CS 4540</h1>
-            <h2>Web Software Architecture - Spring 2016</h2>
-            <h2>Brandon Tobin</h2>
-            <h2>Grad Progress - Assignment 5</h2>
-        </div>
-
-        <!-- Navigation Bar -->
-        <ul id="navigation">
-            <li><a href="../../../../index.html">Home</a></li>
-            <li><a href="../../../">Projects</a></li>
-            <li><a href="../../../../Class_Examples">Examples</a></li>
-            <li><a href="../DGS/overview.php">DGS Overview</a></li>
-        </ul>
 
         <?php
 
