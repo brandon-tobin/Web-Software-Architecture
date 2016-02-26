4fa6af38d3beb7473603911a81c126a83722f103
Brandon Tobin <brandontobin@cox.net>
2016-02-26 15:02:11 -0700

    Creating navbar

--- Projects/Grad_Progress/View/Account/account_home.php
+++ Projects/Grad_Progress/View/Account/account_home.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
+require('../../View/Partials/partial_view.php');
+
 echo "
     <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
 
