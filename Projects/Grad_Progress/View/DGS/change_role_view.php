7fa267160d7ebccdfb57c6c2a0fa24ac7851fd71
Brandon Tobin <brandontobin@cox.net>
2016-02-25 18:24:43 -0700

    Added submit button

--- Projects/Grad_Progress/View/DGS/change_role_view.php
+++ Projects/Grad_Progress/View/DGS/change_role_view.php
@@ -82,6 +82,7 @@
                 <option value="staff">staff</option>
                 <option value="dgs">dgs</option>
             </select>
+            <input type="submit" value="Submit"/>
         </form>
     </body>
 </html>
