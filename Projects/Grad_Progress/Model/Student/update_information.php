cb5577ffaacf709ee811ce1247e746c90b0d7453
Brandon Tobin <brandontobin@cox.net>
2016-02-26 00:15:21 -0700

    Getting user info to update

--- Projects/Grad_Progress/Model/Student/update_information.php
+++ Projects/Grad_Progress/Model/Student/update_information.php
@@ -70,6 +70,8 @@ class Update_Info
 
             $result = $statement->fetchAll(PDO::FETCH_ASSOC);
 
+            error_log("TOBIN RESULT IS : " . $result);
+
             foreach ($result as $row) {
                 $this->position = $row['position'];
                 $this->degree = $row['degree'];
