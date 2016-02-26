5ec55ae8add845f6d75e52f139c707240532f68a
Brandon Tobin <brandontobin@cox.net>
2016-02-26 15:35:03 -0700

    Working on student navbar

--- Projects/Grad_Progress/View/Partials/partial_view.php
+++ Projects/Grad_Progress/View/Partials/partial_view.php
@@ -52,7 +52,15 @@ function getNavBar($role)
     }
     else if (in_array('student', $role))
     {
-
+        return "
+        <!-- Nav Bar -->
+        <ul id=\"navBar\">
+            <li><a>Welcome ".$_SESSION['realname']."</a></li>
+            <li><a href=\"../Account/account_home.php\">Account Home</a></li>
+            <li><a href=\"../Student/update_information.php\">Update Information</a></li>
+            <li><a href=\"../Student/student_forms.php?id=".$_SESSION['userid']."\">Change User Role</a></li>
+            <li><a href=\"logout.php\">Logout</a></li>
+        </ul>";
     }
 
 
