7157666ae53d90088bc1803ef90f59021a8036cc
Brandon Tobin <brandontobin@cox.net>
2016-02-26 18:04:29 -0700

    Student Status

--- Projects/Grad_Progress/Controller/Student/student_status.php
+++ Projects/Grad_Progress/Controller/Student/student_status.php
@@ -14,10 +14,10 @@ set_include_path("../../Model/Student/" .PATH_SEPARATOR . "../../View/Student/")
 require_once 'student_status.php';
 
 // Get the id out of the url
-//$id = $_GET['id'];
+$id = $_GET['id'];
 
 // Create a new student form object
-$status = new New_Student_Status($_SESSION['userid']);
+$status = new New_Student_Status($id);
 
 // Require the form view for display
 require "student_status_view.php";
