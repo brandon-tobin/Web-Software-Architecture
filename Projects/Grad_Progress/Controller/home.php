72739f23cb09b3384d07f635062db25cf5e70567
Brandon Tobin <brandontobin@cox.net>
2016-02-29 16:18:29 -0700

    Testing login logic for nav bar

--- Projects/Grad_Progress/Controller/home.php
+++ Projects/Grad_Progress/Controller/home.php
@@ -1,55 +1,261 @@
-<?php require('../View/Partials/partial_view.php'); ?>
+<?php
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+require ('../Model/Functions/db.php');
+require ('../Model/Functions/authentication.php');
 
-    <html lang="en">
-        <head>
+if (isset($_REQUEST['submit']) && isset($_REQUEST['username']) && isset($_REQUEST['password']))
+{
+    //navBar_Login('');
+    verify_Login('');
+}
 
-            <!-- Last Updated Spring 2016 -->
-            <!-- Brandon Tobin -->
-            <!-- University of Utah -->
-
-            <!-- This is a landing page for Grad Progress  -->
+?>
 
 
-            <title>Grad Progress</title>
 
-            <!-- Meta Information about Page -->
-            <meta charset="utf-8"/>
-            <meta name="AUTHOR"      content="Brndon Tobin"/>
-            <meta name="keywords"    content="HTML, Projects"/>
-            <meta name="description" content="Landing page for Grad Progress"/>
 
-            <!-- ALL CSS FILES -->
-            <link rel="stylesheet" href="../../../Resources/css/stylesheet.css" type="text/css"/>
+<!DOCTYPE html>
+<html lang="en">
 
-        </head>
+<head>
 
-        <body>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-        <?php
+    <title>Landing Page - Start Bootstrap Theme</title>
 
-        echo (getHeader());
+    <!-- Bootstrap Core CSS -->
+    <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
+    <link href="../../../Resources/Bootstrap/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet">
 
-        echo (getNavigation());
+    <!-- Custom CSS -->
+    <link href="../../../Resources/Bootstrap/bootstrap-3.3.6-dist/css/landing-page.css" rel="stylesheet">
 
-        echo (getNavBar(''));
+    <!-- Custom Fonts -->
+    <link href="../../../Resources/Bootstrap/bootstrap-3.3.6-dist/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
-        echo (pageDataHeader("Welcome to the Graduate Student System!"));
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-        if (isset($_SESSION['userid']))
-        {
-            echo "<h2>Welcome back ".$_SESSION['realname']."! Please click on a link below to continue.</h2>";
-        }
+</head>
 
-        ?>
+<body>
 
-        <p>Below are some actions you can do.</p>
+<!-- Navigation -->
+<nav class="navbar navbar-default navbar-fixed-top topnav" role="navigation">
+    <div class="container topnav">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand topnav">Welcome to the Grad Student System</a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#about">About</a>
+                </li>
+                <li>
+                    <a href="#services">Services</a>
+                </li>
+                <li>
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group">
+                    <input type="text" class="form-control" name="username" placeholder="Username">
+                </div>
+                <div class="form-group">
+                    <input type="text" class="form-control" name="password" placeholder="Password">
+                </div>
+                <button type="submit" name="submit" class="btn btn-default">Sign In</button>
+            </form>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
 
-        <ul><a href="Account/account_home.php">Account Home</a></ul>
-        <ul><a href="Account/creation_form.php">Register</a></ul>
 
-        </body>
+<!-- Header -->
+<a name="about"></a>
+<div class="intro-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="intro-message">
+                    <h1>School of Computing</h1>
+                    <h3>Graduate Student System</h3>
+                    <hr class="intro-divider">
+                    <ul class="list-inline intro-social-buttons">
+                        <li>
+                            <a href="Account/account_home.php" class="btn btn-default btn-lg"><span class="network-name">Account Home</span></a>
+                        </li>
+                        <li>
+                            <a href="Account/creation_form.php" class="btn btn-default btn-lg"><span class="network-name">Register</span></a>
+                        </li>
+                        <!--<li>
+                            <a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                        </li>-->
+                    </ul>
+                </div>
+            </div>
+        </div>
 
-    </html>
+    </div>
+    <!-- /.container -->
 
+</div>
+<!-- /.intro-header -->
+
+<!-- Page Content -->
+
+<a  name="services"></a>
+<div class="content-section-a">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-sm-6">
+                <hr class="section-heading-spacer">
+                <div class="clearfix"></div>
+                <h2 class="section-heading">Account Home</h2>
+                <p class="lead">Account Home will take you to your account homepage where you can perform specific tasks to your user account like viewing and creating student forms.</p>
+            </div>
+            <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+                <!--<img class="img-responsive" src="img/ipad.png" alt="">-->
+                <!--<span class="glyphicon glyphicon-home" aria-hidden="true"></span>-->
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+</div>
+<!-- /.content-section-a -->
+
+<div class="content-section-b">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                <hr class="section-heading-spacer">
+                <div class="clearfix"></div>
+                <h2 class="section-heading">Register</h2>
+                <p class="lead">Register will take you to the new account registration page which will allow you to create a new user account in the Graduate Student System.</p>
+            </div>
+            <div class="col-lg-5 col-sm-pull-6  col-sm-6">
+                <!--<img class="img-responsive" src="img/dog.png" alt="">-->
+                <!--<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>-->
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+</div>
+<!-- /.content-section-b -->
+
+<!--<div class="content-section-a">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-5 col-sm-6">
+                <hr class="section-heading-spacer">
+                <div class="clearfix"></div>
+                <h2 class="section-heading">Google Web Fonts and<br>Font Awesome Icons</h2>
+                <p class="lead">This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</p>
+            </div>
+            <div class="col-lg-5 col-lg-offset-2 col-sm-6">
+                <img class="img-responsive" src="img/phones.png" alt="">
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+</div>
+<!-- /.content-section-a -->
+
+<!--<a  name="contact"></a>
+<div class="banner">
+
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>Connect to Start Bootstrap:</h2>
+            </div>
+            <div class="col-lg-6">
+                <ul class="list-inline banner-social-buttons">
+                    <li>
+                        <a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    </li>
+                    <li>
+                        <a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    </li>
+                    <li>
+                        <a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+<!--</div>
+<!-- /.banner -->
+
+<!-- Footer -->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="list-inline">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li class="footer-menu-divider">&sdot;</li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li class="footer-menu-divider">&sdot;</li>
+                    <li>
+                        <a href="#services">Services</a>
+                    </li>
+                    <li class="footer-menu-divider">&sdot;</li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+                <p class="copyright text-muted small">Copyright &copy; Brandon Tobin. All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
