eeac9b2829d2fc8338525191bf5c4a247552005c
Brandon Tobin <brandontobin@cox.net>
2016-03-03 14:52:50 -0700

    updated student navbar

--- Projects/Grad_Progress/View/Partials/partial_view.php
+++ Projects/Grad_Progress/View/Partials/partial_view.php
@@ -118,37 +118,37 @@ function getNavBar($role)
     {
         return "
 
-    <!-- Navigation -->
-    <nav class=\"navbar navbar-custom \" role=\"navigation\">
-        <div class=\"container\">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class=\"navbar-header\">
-                <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\">
-                    <span class=\"sr-only\">Toggle navigation</span>
-                    <span class=\"icon-bar\"></span>
-                    <span class=\"icon-bar\"></span>
-                    <span class=\"icon-bar\"></span>
-                </button>
-                <a class=\"navbar-brand\" href=\"#\">Welcome ".$_SESSION['realname']."</a>
+        <!-- Navigation -->
+        <nav class=\"navbar navbar-custom \" role=\"navigation\">
+            <div class=\"container\">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class=\"navbar-header\">
+                    <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\">
+                        <span class=\"sr-only\">Toggle navigation</span>
+                        <span class=\"icon-bar\"></span>
+                        <span class=\"icon-bar\"></span>
+                        <span class=\"icon-bar\"></span>
+                    </button>
+                    <a class=\"navbar-brand\" href=\"#\">Welcome ".$_SESSION['realname']."</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
+                    <ul class=\"nav navbar-nav\">
+                        <li>
+                            <a href=\"../Account/account_home.php\">Account Home</a>
+                        </li>
+                        <li>
+                            <a href=\"../Advisor/students.php?id=".$_SESSION['userid']."\">View Students</a>
+                        </li>
+                        <li>
+                            <a href=\"../Account/logout.php\">Logout</a></a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
             </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
-                <ul class=\"nav navbar-nav\">
-                    <li>
-                        <a href=\"../Account/account_home.php\">Account Home</a>
-                    </li>
-                    <li>
-                        <a href=\"../Advisor/students.php?id=".$_SESSION['userid']."\">View Students</a>
-                    </li>
-                    <li>
-                        <a href=\"../Account/logout.php\">Logout</a></a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>";
+            <!-- /.container -->
+        </nav>";
 
         /*(return "
         <!-- Nav Bar -->
@@ -166,6 +166,49 @@ function getNavBar($role)
     else if (in_array('student', $role))
     {
         return "
+        <!-- Navigation -->
+        <nav class=\"navbar navbar-custom \" role=\"navigation\">
+            <div class=\"container\">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class=\"navbar-header\">
+                    <button type=\"button\" class=\"navbar-toggle\" data-toggle=\"collapse\" data-target=\"#bs-example-navbar-collapse-1\">
+                        <span class=\"sr-only\">Toggle navigation</span>
+                        <span class=\"icon-bar\"></span>
+                        <span class=\"icon-bar\"></span>
+                        <span class=\"icon-bar\"></span>
+                    </button>
+                    <a class=\"navbar-brand\" href=\"#\">Welcome ".$_SESSION['realname']."</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
+                    <ul class=\"nav navbar-nav\">
+                        <li>
+                            <a href=\"../Account/account_home.php\">Account Home</a>
+                        </li>
+                        <li>
+                            <a href=\"../Student/update_information.php\">Update Information</a>
+                        </li>
+                        <li>
+                            <a href=\"../Student/student_forms.php?id=".$_SESSION['userid']."\">View Forms</a>
+                        </li>
+                        <li>
+                            <a href=\"../Student/student_status.php?id=".$_SESSION['userid']."\">Student Status</a>
+                        </li>
+                        <li>
+                            <a href=\"../Student/student_status_update.php?id=".$_SESSION['userid']."\">Update Student Status</a>
+                        </li>
+                        <li>
+                            <a href=\"../Account/logout.php\">Logout</a></a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container -->
+        </nav>";
+
+
+        /*return "
         <!-- Nav Bar -->
         <ul id=\"navBar\">
             <li><Welcome ".$_SESSION['realname']."</li>
@@ -175,7 +218,7 @@ function getNavBar($role)
             <li><a href=\"../Student/student_status.php?id=".$_SESSION['userid']."\">Student Status</a></li>
             <li><a href=\"../Student/student_status_update.php?id=".$_SESSION['userid']."\">Update Student Status</a></li>
             <li><a href=\"../Account/logout.php\">Logout</a></li>
-        </ul>";
+        </ul>";*/
     }
     else if (in_array('register', $role))
     {
