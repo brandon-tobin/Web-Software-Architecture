390c91a34dfb78aa3d51452a39bb4bdab5c7d62f
Brandon Tobin <brandontobin@cox.net>
2016-03-03 13:34:02 -0700

    Configuring boostrap navbar

--- Projects/Grad_Progress/View/Partials/partial_view.php
+++ Projects/Grad_Progress/View/Partials/partial_view.php
@@ -61,19 +61,19 @@ function getNewNavigation()
                     <span class=\"icon-bar\"></span>
                     <span class=\"icon-bar\"></span>
                 </button>
-                <a class=\"navbar-brand\" href=\"#\">Start Bootstrap</a>
+                <a class=\"navbar-brand\" href=\"#\">Website Links</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
                 <ul class=\"nav navbar-nav\">
                     <li>
-                        <a href=\"#\">About</a>
+                        <a href=\"../../../../index.html\">Home</a>
                     </li>
                     <li>
-                        <a href=\"#\">Services</a>
+                        <a href=\"../../../../Projects/\">Projects</a>
                     </li>
                     <li>
-                        <a href=\"#\">Contact</a>
+                        <a href=\"../../../../Class_Examples/\">Examples</a>
                     </li>
                 </ul>
             </div>
