ebaf0137861b7d9e4ba7cc5ba94ad4d5f3b4064d
Brandon Tobin <brandontobin@cox.net>
2016-03-05 16:43:46 -0700

    Changing buttons to use bootstrap

--- Projects/Grad_Progress/View/Student/update_information_view.php
+++ Projects/Grad_Progress/View/Student/update_information_view.php
@@ -46,7 +46,7 @@
 
         <form method="post" action="">
             <div class="table-responsive">
-                <table class="table table-hover">
+                <table class="table table-condensed table-hover">
                     <tr>
                         <td>Full Name:</td>
                         <td><?php echo $info->name?></td>
