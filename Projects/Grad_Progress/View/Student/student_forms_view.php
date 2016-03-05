46902163e5ea5f934c543981e0810271e4695c16
Brandon Tobin <brandontobin@cox.net>
2016-03-05 16:23:03 -0700

    Changing buttons to use bootstrap

--- Projects/Grad_Progress/View/Student/student_forms_view.php
+++ Projects/Grad_Progress/View/Student/student_forms_view.php
@@ -111,7 +111,10 @@ echo "
 
                 <h1>Create New Form</h1>
                 <!--<p><a href='new_progress_form.php?id=$student->student_ID'>Create Form</a></p>-->
-                <a class=\"btn btn-success\" href='new_progress_form.php?id=$student->student_ID' role=\"button\" >Create Form</a>
+                <a class=\"btn btn-success btn-lg\" href='new_progress_form.php?id=$student->student_ID' role=\"button\" >
+                Create Form
+                <span class=\"glyphicon glyphicon-list\" aria-hidden=\"true\"></span>
+                </a>
 
             </body>
 
