dce5c37d9c3cd09d397e120bc0b655f223aef1fa
Brandon Tobin <brandontobin@cox.net>
2016-03-04 18:17:57 -0700

    Adding breadcrumbs for multiple pages

--- Projects/Grad_Progress/View/Student/edit_progress_form_view.php
+++ Projects/Grad_Progress/View/Student/edit_progress_form_view.php
@@ -57,6 +57,17 @@ echo "
                             <li class=\"active\">Edit Process Form</li>
                         </ol>";
                 }
+                else if (in_array('faculty', $_SESSION['roles']))
+                {
+                    echo "
+                    <!-- Breadcrumb -->
+                    <ol class=\"breadcrumb\">
+                        <li><a href=\"../Account/account_home.php\">Account Home</a></li>
+                        <li><a href=\"../Advisor/students.php?id=".$_SESSION['userid']."\">My Graduate Students</a></li>
+                        <li><a href=\"../Student/student_forms.php?id=".$_SESSION['student']."\">$form->student_Name's Forms</a></li>
+                        <li class=\"active\">Edit Process Form</li>
+                    </ol>";
+                }
 
                 echo (pageDataHeader("Due Progress Advisory Document for Ph.D. Degree"));
 
