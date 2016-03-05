7d5dc1eee776f861e28d879e1b01158049303525
Brandon Tobin <brandontobin@cox.net>
2016-03-05 14:14:41 -0700

    Changing tables

--- Projects/Grad_Progress/View/DGS/overview_view.php
+++ Projects/Grad_Progress/View/DGS/overview_view.php
@@ -57,7 +57,7 @@ echo "
             <h1>Graduate Advisors</h1>
 
             <!-- Table containing advisors -->
-            <table class=\"roster\">
+            <table class=\"table table-striped\">
                 <tr>
                     <th>Name:</th>
                     <th>Profile:</th>
