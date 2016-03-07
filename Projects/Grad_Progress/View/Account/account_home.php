a131dc329644b34fbd42a65f8c0245afcb6f6401
Brandon Tobin <brandontobin@cox.net>
2016-03-07 16:35:33 -0700

    Adding bootstrap page headers

--- Projects/Grad_Progress/View/Account/account_home.php
+++ Projects/Grad_Progress/View/Account/account_home.php
@@ -59,7 +59,7 @@ echo "
     <div class=\"container-fluid\">
         <div class=\"row\">
         <div class=\"col-md-3\"></div>
-        <div class=\"col-md-6\"></div>
+        <div class=\"col-md-6\"></div
 
         ";
 
