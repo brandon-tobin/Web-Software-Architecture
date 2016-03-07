85eb5b806b50ba5bf841af5f8d36eaf1a95e58c5
Brandon Tobin <brandontobin@cox.net>
2016-03-07 16:48:25 -0700

    Trying to implement bootstrap grid system

--- Projects/Grad_Progress/View/Account/account_home.php
+++ Projects/Grad_Progress/View/Account/account_home.php
@@ -46,8 +46,8 @@ echo "
 
     <div class=\"container-fluid\">
         <div class=\"row\">
-        <div class=\"col-md-3\"></div>
-        <div class=\"col-md-6\">
+        <div class=\"col-md-2\"></div>
+        <div class=\"col-md-8\">
 
         ";
 echo (getHeader());
