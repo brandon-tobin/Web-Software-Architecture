9b5b7b30f1cd1956237fb3278c51e2c6beef09f7
Brandon Tobin <brandontobin@cox.net>
2016-03-09 15:41:16 -0700

    Adding grids / footer

--- Projects/Grad_Progress/View/DGS/overview_view.php
+++ Projects/Grad_Progress/View/DGS/overview_view.php
@@ -38,7 +38,14 @@ echo "
 
         </head>
 
-        <body>";
+        <body>
+
+        <div class=\"container-fluid\">
+            <div class=\"row\">
+                <div class=\"col-sm-0 col-md-1 col-lg-2\"></div>
+                <div class=\"col-sm-12 col-md-10 col-lg-8\">
+        ";
+
 
         echo (getHeader());
 
@@ -104,10 +111,17 @@ echo "
                     echo "
 
                 </table>
-            </div>
+            </div>";
+
+            echo (getFooter());
+
+        echo "
+
+         </div> <!-- Ending column -->
+         <div class=\"col-sm-0 col-md-1 col-lg-2\"></div>
+    </div> <!-- Ending Row -->
 
         </body>
 
     </html>
-
 ";
