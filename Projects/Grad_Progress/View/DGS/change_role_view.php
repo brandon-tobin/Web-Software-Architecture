70171f3a1cd6bbd08874fc305dc897d5da9fc97f
Brandon Tobin <brandontobin@cox.net>
2016-03-09 15:35:53 -0700

    Adding grids / footer

--- Projects/Grad_Progress/View/DGS/change_role_view.php
+++ Projects/Grad_Progress/View/DGS/change_role_view.php
@@ -98,7 +98,7 @@
             </button>
         </form>
 
-        <? echo (getFooter()); ?>
+        <?php echo (getFooter()); ?>
 
         </div> <!-- Ending Column -->
         </div class="col-sm-0 col-md-1 col-lg-2"></div>
