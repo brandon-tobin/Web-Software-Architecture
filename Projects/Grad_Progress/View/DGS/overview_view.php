146d0ba22f592d1222aac0c214af09c563a679e8
Brandon Tobin <brandontobin@cox.net>
2016-03-22 15:55:09 -0600

    Playing around with AJAX

--- Projects/Grad_Progress/View/DGS/overview_view.php
+++ Projects/Grad_Progress/View/DGS/overview_view.php
@@ -47,7 +47,9 @@ echo "
             <div class=\"row\">
                 <div class=\"col-sm-0 col-md-1 col-lg-2\"></div>
                 <div class=\"col-sm-12 col-md-10 col-lg-8\">
-        ";
+
+
+                ";
 
 
         echo (getHeader());
