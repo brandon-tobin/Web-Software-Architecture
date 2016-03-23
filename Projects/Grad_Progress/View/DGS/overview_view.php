ed2d08bcb10bd5182dbc595bb12973a8ac2cf1d8
Brandon Tobin <brandontobin@cox.net>
2016-03-23 09:26:46 -0600

    Trying to get highchart to display

--- Projects/Grad_Progress/View/DGS/overview_view.php
+++ Projects/Grad_Progress/View/DGS/overview_view.php
@@ -41,6 +41,137 @@ echo "
         <script type=\"text/javascript\" src=\"../../../../Resources/Highcharts/api/js/jquery-1.11.3.min.js\"></script>
         <script type=\"text/javascript\" src=\"../../../../Resources/Highcharts/js/highcharts.src.js\"></script>
 
+        <script>
+        function find_data(  )
+{
+
+    $.ajax(
+        {
+            type:'POST',
+            //url:  $(\"input[name=cause_error]\").is(':checked') ? \"asdf\" : \"get_data.php\",
+            url: \"../../../../Resources/get_data.php\",
+            data: $('#form_id').serialize(),
+            dataType: \"text\",  		      // The type of data that is getting returned.
+
+            success: function(response)
+            {
+                // note: this function should be removed and use only the done function below
+                console.log(\"success function\");
+            },
+
+            /**
+             * What to do before the ajax request is sent. Perhaps gather
+             * page information, prep form, etc.
+             */
+            beforeSend: function()
+            {
+                //var check_box = $(\"input[name=before_send]\");
+                //
+                //if (check_box.is(':checked'))
+                //{
+                //    alert ( \"prepping AJAX call with data: \" + $('#form_id').serialize() );
+                //}
+
+            },
+
+        })
+        .done( function ( data )
+        {
+            //console.log(\"done function\")
+            //var check_box = $(\"input[name=on_success]\");
+            //
+            ///**
+            // * What to do when the data is successfully retreived
+            // */
+            //if (check_box.is(':checked'))
+            //{
+            //    alert ( \"Data Rerturned Successfully!\" );
+            //}
+
+           // var jContent = $( \"#content\" ); // put data here
+
+
+            //$(\"#content\").html(data);
+           // $(\"#content\").find(\"script\").each(function(i) {
+             //   eval($(this).text());
+
+               // jContent.script(data);
+
+           // jContent.html( data );
+
+            data_series[0].data.push(data);
+
+
+
+
+        } )
+        .fail( function ( text, options, err )
+        {
+            /**
+             * What to do if there is an error
+             *
+             */
+            // something went wrong
+            var jContent = $( \"#content\" );
+            jContent.html(text + \"   \" + options +\"   \" + err);
+            console.log('Jim, error message: ' + text );
+            console.log('Jim, error message: ' + options );
+            console.log('Jim, error message: ' + err);
+        } )
+        .always( function ( )
+        {
+            /**
+             * What to do no matter what
+             *
+             */
+            console.log('Jim, cleaning up');
+        } );
+
+    // if this
+    // disable a page submit
+    //return false;
+}
+
+$(document).ready(function () {
+    find_data();
+
+    $('#linechart').highcharts({
+        chart: {type: 'column'},
+        title: {
+            text: 'GPAs',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: Jim',
+            x: -20
+        },
+        xAxis: {
+            title: 'credit_hours',
+        },
+        yAxis: {
+            min: 0, max: 4,
+            title: {
+                text: 'GPA'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{\"name\": \"GPAS\", \"data\": data_series}]
+    });
+});
+</script>
+
+
+
         </head>
 
         <body>
