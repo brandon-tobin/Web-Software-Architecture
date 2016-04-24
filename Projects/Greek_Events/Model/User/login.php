bf9723f4d637c5b6302ed03690e1270895dbdc98
Brandon Tobin <brandontobin@cox.net>
2016-04-24 16:54:20 -0600

    Added basic login functionality

---
+++ Projects/Greek_Events/Model/User/login.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: Brandon Tobin
+ * Date: Spring 2016
+ *
+ * Login Model
+ *
+*/
+
+require ('../../Model/Functions/db.php');
+
+require ('../../Model/Functions/authentication.php');
+
+verify_Login('');
