c22d6d519ada468eb33c66929c9c963ff11da708
Brandon Tobin <brandontobin@cox.net>
2016-04-24 17:21:39 -0600

    Added basic login functionality

--- Projects/Greek_Events/View/Event/view_event_view.php
+++ Projects/Greek_Events/View/Event/view_event_view.php
@@ -53,7 +53,7 @@ echo "
                             <!-- Attending Button Group -->
                             <form method=\"post\">
                             <div class=\"btn-group btn-group-lg\" role=\"group\">
-                              <button type=\"submit\" class=\"btn btn-success\" value=\"Attending\">Attending</button>
+                              <button type=\"submit\" class=\"btn btn-success\" name=\"Attending\" value=\"Attending\">Attending</button>
                               <button type=\"button\" class=\"btn btn-warning\">Maybe Attending</button>
                               <button type=\"button\" class=\"btn btn-danger\">Not Attending</button>
                             </div>
