bac39d04de4c94c26e9228c86faffeec57ddfac9
Brandon Tobin <brandontobin@cox.net>
2016-04-25 16:55:14 -0600

    Adding code for comments on events

--- Projects/Greek_Events/View/Event/view_event_view.php
+++ Projects/Greek_Events/View/Event/view_event_view.php
@@ -37,8 +37,8 @@ echo "
 
                 <div class=\"container - fluid\">
                     <div class=\"row\">
-                        <div class=\"col-sm-0 col-md-1 col-lg-2\"></div>
-                        <div class=\"col-sm-8 col-md-8 col-lg-8\">
+                        <div class=\"col-sm-0 col-md-1 col-lg-1\"></div>
+                        <div class=\"col-sm-12 col-md-10 col-lg-10\">
 
                             <h1>View Event</h1>
 
@@ -168,7 +168,7 @@ echo "
                                 </div>
                             </div>
                         </div>
-                        <div class=\"col-sm-0 col-md-1 col-lg-2\"></div>
+                        <div class=\"col-sm-0 col-md-1 col-lg-1\"></div>
                     </div>
                 </div>
 
