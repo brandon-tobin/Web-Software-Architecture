4865d8e28beeba61528c007a3fa0775c9064e094
Brandon Tobin <brandontobin@cox.net>
2016-04-25 17:01:23 -0600

    Working on bootstrap

--- Projects/Greek_Events/View/Event/view_event_view.php
+++ Projects/Greek_Events/View/Event/view_event_view.php
@@ -37,8 +37,8 @@ echo "
 
                 <div class=\"container - fluid\">
                     <div class=\"row\">
-                        <div class=\"col-sm-0 col-md-1 col-lg-1\"></div>
-                        <div class=\"col-sm-12 col-md-10 col-lg-10\">
+                        <div class=\"col-sm-0 col-md-1 col-lg-2\"></div>
+                        <div class=\"col-sm-12 col-md-10 col-lg-8\">
 
                             <h1>View Event</h1>
 
