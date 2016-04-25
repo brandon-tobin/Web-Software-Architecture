a99fcd18c0122cd98d660159708a885b244de9c8
Brandon Tobin <brandontobin@cox.net>
2016-04-25 17:10:26 -0600

    Working on bootstrap

--- Projects/Greek_Events/View/Event/view_event_view.php
+++ Projects/Greek_Events/View/Event/view_event_view.php
@@ -133,8 +133,8 @@ echo "
                                     </div>
 
                                     <h2>Comments:</h2>
-                                    <div class=\"table - responsive\">
-                                        <table class=\"table table - striped table - bordered table - condensed\">
+                                    <div class=\"table-responsive\">
+                                        <table class=\"table table-striped table-bordered table-condensed col-sm-12 col-md-10 col-lg-8\">
                                             <tr>
                                                 <th>Name:</th>
                                                 <th>Comment:</th>
