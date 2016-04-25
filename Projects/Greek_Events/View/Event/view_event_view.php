cf9060e50a6fe4e374926fea4f6403b30bb951d4
Brandon Tobin <brandontobin@cox.net>
2016-04-25 15:21:37 -0600

    Adding code for comments on events

--- Projects/Greek_Events/View/Event/view_event_view.php
+++ Projects/Greek_Events/View/Event/view_event_view.php
@@ -156,15 +156,47 @@ echo "
 
                                     <!--<h2>Leave a Comment:</h2>-->
                                     <!-- Attending Button Group -->
-                                   <!-- <div class=\"container\">-->
+                                    <div class=\"container\">
                                         <h2>Leave a Comment</h2>
-                                        <form role=\"form\">
-                                            <div class=\"comment-form\">
+                                        <form method=\"POST\">
+                                            <div class=\"form-group\">
                                                 <label for=\"comment\">Comment:</label>
-                                                <textarea class=\"comment-form\" rows=\"5\" id=\"comment\"></textarea>
+                                                <textarea class=\"form-control\" rows=\"5\" id=\"comment\"></textarea>
                                             </div>
                                         </form>
-                                    <!--</div>-->
+                                    </div>
+
+
+
+
+
+
+
+
+                                    <form method=\"post\">
+
+                                        <div class=\"form-group\">
+                                            <label for=\"comment\">Comment</label>
+                                            <input type=\"textarea\" class=\"form-control\" id=\"exampleInputEmail1\" placeholder=\"Email\">
+                                        </div>
+                                      <div class=\"form-group\">
+                                        <label for=\"exampleInputPassword1\">Password</label>
+                                        <input type=\"password\" class=\"form-control\" id=\"exampleInputPassword1\" placeholder=\"Password\">
+                                      </div>
+                                      <div class=\"form-group\">
+                                        <label for=\"exampleInputFile\">File input</label>
+                                        <input type=\"file\" id=\"exampleInputFile\">
+                                        <p class=\"help-block\">Example block-level help text here.</p>
+                                      </div>
+                                      <div class=\"checkbox\">
+                                        <label>
+                                          <input type=\"checkbox\"> Check me out
+                                        </label>
+                                      </div>
+                                      <button type=\"submit\" class=\"btn btn-default\">Submit</button>
+                                    </form>
+                                    </form>
+
                                 </div>
                             </div>
                         </div>
