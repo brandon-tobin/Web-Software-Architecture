4a7405032d1c5025ccfd5e3727a5c76a7def88ec
Brandon Tobin <brandontobin@cox.net>
2016-04-25 21:39:38 -0600

    Adding bootstrap to new event page

--- Projects/Greek_Events/View/Event/new_event_view.php
+++ Projects/Greek_Events/View/Event/new_event_view.php
@@ -6,6 +6,8 @@
  * Time: 9:28 PM
  */
 
+require('../../View/Partials/partial_view.php');
+
 echo "
         <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\" \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
 
