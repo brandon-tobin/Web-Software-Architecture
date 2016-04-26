99d990328726cab703d484477fc93fc8cb3b299f
Brandon Tobin <brandontobin@cox.net>
2016-04-25 21:32:49 -0600

    Adding nav bar

--- Projects/Greek_Events/View/Event/view_all_events_view.php
+++ Projects/Greek_Events/View/Event/view_all_events_view.php
@@ -36,7 +36,7 @@ echo "
             </head>
 
             <body>
-                <div class=\"container - fluid\">
+                <div class=\"container-fluid\">
                     <div class=\"row\">
                         <div class=\"col-sm-0 col-md-1 col-lg-2\"></div>
                         <div class=\"col-sm-12 col-md-10 col-lg-8\">";
