f7d19d2c871ddbcdf0d078d30d3e817e36514e0c
aoki19 <aoki19@yahoo.com>
2016-04-29 14:48:54 -0600

    homepage

---
+++ Projects/Greek_Events/Controller/Home/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fumiko
+ * Date: 4/29/2016
+ * Time: 2:47 PM
+ */
+
+// Set the include path for the model and view
+set_include_path("../../Model/Home/");
+
+// Require the model file once
+require_once 'index.php';
