b79aec728d5b6d5d0e81e65261265948139e995c
aoki19 <aoki19@yahoo.com>
2016-05-01 13:34:37 -0600

    new user creation

--- Projects/Greek_Events/Model/Home/new_user.php
+++ Projects/Greek_Events/Model/Home/new_user.php
@@ -13,36 +13,48 @@ require '../../Model/Functions/authentication.php';
 verify_Login("");
 
 if (isset($_POST['submit'])) {
-    $username = trim($_REQUEST['username']);
-    $password = trim($_REQUEST['password']);
-    $cpassword = trim($_REQUEST['cpassoword']);
-    $name = trim($_REQUEST['name']);
-    $organization = trim($_REQUEST['organization']);
-    $orgID;
 
-    $db = openDBConnection();
-    $query = "Select orgID from Organizations where name = ?";
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(1, $organization);
-    $stmt->execute();
-    $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+    try {
+        $username = trim($_REQUEST['username']);
+        $password = trim($_REQUEST['password']);
+        $cpassword = trim($_REQUEST['cpassoword']);
+        $name = trim($_REQUEST['name']);
+        $organization = trim($_REQUEST['organization']);
+        $orgID;
 
-    foreach ($result as $row) {
-        $this->orgID = htmlspecialchars($row['orgID']);
+        $db = openDBConnection();
+        $query = "Select orgID from Organizations where name = ?";
+        $stmt = $db->prepare($query);
+        $stmt->bindValue(1, $organization);
+        $stmt->execute();
+        $result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($result as $row) {
+            $this->orgID = htmlspecialchars($row['orgID']);
+        }
+
+        error_log("ANNE: orgID is {$orgID}");
+
+        $db->beginTransaction();
+        $stmt = $db->prepare("INSERT INTO User (username, password, name, orgID) values (?, ?, ?, ?)");
+        $stmt->bindValue(1, $username);
+        $stmt->bindValue(2, $password);
+        $stmt->bindValue(3, $name);
+        $stmt->bindValue(4, $orgID);
+        $stmt->execute();
+        $db->commit();
+
+        $_SESSION['realname'] = htmlspecialchars($row['name']);
+        $_SESSION['login'] = $username;
+        $_SESSION['roles'] = htmlspecialchars($row['account_level']);
+    }
+    catch (PDOException $ex)
+    {
+        error_log("ANNE: error creating user : " . $ex->getMessage());
+        exit();
     }
 
-    $db->beginTransaction();
-    $stmt = $db->prepare("INSERT INTO User (username, password, name, orgID) values (?, ?, ?, ?)");
-    $stmt->bindValue(1, $username);
-    $stmt->bindValue(2, $password);
-    $stmt->bindValue(3, $name);
-    $stmt->bindValue(4, $orgID);
-    $stmt->execute();
-    $db->commit();
-
-    $_SESSION['realname'] = htmlspecialchars($row['name']);
-    $_SESSION['login'] = $username;
-    $_SESSION['roles'] = htmlspecialchars($row['account_level']);
+    require_once "../../Controller/User/success.php";
 }
 
 class NewUser
