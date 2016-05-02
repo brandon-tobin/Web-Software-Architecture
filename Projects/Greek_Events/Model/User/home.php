f7825c58b080e3c15140e61d5c38e04faad6d635
aoki19 <aoki19@yahoo.com>
2016-05-01 21:55:04 -0600

    homepage

--- Projects/Greek_Events/Model/User/home.php
+++ Projects/Greek_Events/Model/User/home.php
@@ -82,7 +82,7 @@ class home
                 }
 
                 //$this->events[] = array($author_Name, $author_Username, $author_Organization, $event_Name, $event_Date, $event_Description, $event_Location, "<a href=\"view_event.php?id=$author_Username&event=".htmlspecialchars($available_events[$i])."\">View</a>");
-                $this->events[] = array($author_Name, $author_Username, $author_Organization, $event_Name, $event_Date, $event_Description, $event_Location, "view_event.php?id=$author_Username&event=".htmlspecialchars($available_events[$i])."", $rsvp);
+                $this->events[] = array($author_Name, $author_Username, $author_Organization, $event_Name, $event_Date, $event_Description, $event_Location, "../Event/view_event.php?id=$author_Username&event=".htmlspecialchars($available_events[$i])."", $rsvp);
 
             }
 
